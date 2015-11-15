8e438f2e090b4986c779921f6c9ffe12f1aeccac
Iñaki Nicolàs Pinkevicius <inakipinke@gmail.com>
2015-11-15 13:45:11 -0300

    Modificado el archivo bomberman.py y añadida funcion de colocar bombas

--- Bomberman.py
+++ Bomberman.py
@@ -2,23 +2,23 @@ import pilasengine
 import os
 import sys
 
+
 pilas=pilasengine.iniciar()
 
 
 
-#mapa = pilas.actores.MapaTiled('mapa_desde_archivo.tmx')
+mapa = pilas.actores.MapaTiled('mapa_desde_archivo.tmx')
 
 #Cada bloque mide 30
 
-maton=pilas.actores.Maton(x=0,y=100)
+class maton(pilasengine.actores.Maton):
+    maton=pilas.actores.Maton(x=0,y=100)
+    maton.figura.x=265
+    def bomb(self):
+        if (pilas.control.boton):
+            bomba=pilas.actores.Bomba
+            bomba.explotar()
 
-caja=pilas.actores.Caja(x=60)
-caja.escala=0.63
-
-bomba=pilas.actores.Bomba()
-bomba.radio_de_colision=45
-bomba.escala=0.60
 pilas.fisica.gravedad_y=0
-#pilas.colisiones.eliminar(caja,maton)
 
 pilas.ejecutar()
--- pj/pj1.py
+++ pj/pj1.py
@@ -1,13 +1,14 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+import time
 import pilasengine
 pilas=pilasengine.iniciar()
 pilas.fondos.Noche()
 
 class bomb1(pilasengine.actores.Actor):
     def iniciar(self):
-        self.imagen = "bombita.png"
+        self.imagen="a.png"
     
     def actualizar(self):
         if (pilas.control.izquierda):
@@ -21,6 +22,11 @@ class bomb1(pilasengine.actores.Actor):
         
         if (pilas.control.arriba):
             self.y += 8
+
+        if (pilas.control.boton):
+            bomba=pilas.actores.Bomba(x=self.x,y=self.y)
+            time.sleep(2)
+            bomba.explotar()
 
 bombita=bomb1(pilas)
 pilas.ejecutar()
